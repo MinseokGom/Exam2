7d3f133f5b444d49d368a4f1548405be45adad9a
t_asano <t_asano@LAPTOP-6LFOSQ8U>
2024-04-15 12:00:56 +0900

    DAOファイル

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,20 @@
+
+package dao;
+
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DAO {
+	static DataSource ds;
+
+	// データベースの接続に必要な処理をgetConnectionというメソッドにまとめておく
+	public Connection getConnection() throws Exception {
+		if (ds==null) {
+			InitialContext ic=new InitialContext();
+			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/jissyu1");
+		}
+		return ds.getConnection();
+	}
+}
