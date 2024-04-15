34807ae803171d717329bf03fe969b58cec03863
t_asano <t_asano@LAPTOP-6LFOSQ8U>
2024-04-15 12:14:00 +0900

    DAOファイル

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -9,7 +9,6 @@ import javax.sql.DataSource;
 public class DAO {
 	static DataSource ds;
 
-	// データベースの接続に必要な処理をgetConnectionというメソッドにまとめておく
 	public Connection getConnection() throws Exception {
 		if (ds==null) {
 			InitialContext ic=new InitialContext();
