ba86350098c100fdb0d08eb344a4894f63489e8c
n_oka <n_oka@10.234.146.51>
2024-04-26 10:09:32 +0900

    オラ

--- src/main/java/bean/Teacher.java
+++ src/main/java/bean/Teacher.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 package bean;
 
@@ -42,47 +42,4 @@ public class Teacher implements java.io.Serializable {
 	}
 	
 }
-=======
-package bean;
 
-public class Teacher implements java.io.Serializable {
-   
-	private String id;
-	private String name;
-	private String password;
-	private School school;
-	
-	public String getId() {
-		return id ;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public School getSchool() {
-		return school;
-	}
-	
-	public void setId(String id) {
-		this.id=id;
-	}
-	
-	public void setName(String name) {
-		this.name=name;
-	}
-	
-	public void setPassword(String password) {
-		this.password=password;
-	}
-	
-	public void setSchool(School school) {
-		this.school=school;
-	}
-	
-}
->>>>>>> branch 'master' of https://github.com/MinseokGom/Exam2.git
