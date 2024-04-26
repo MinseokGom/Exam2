7d98b4f219cf81b20ae63c0075278ed52460b917
n_oka <n_oka@10.234.146.51>
2024-04-26 11:58:36 +0900

    08065029384

--- src/main/java/bean/Student.java
+++ src/main/java/bean/Student.java
@@ -8,58 +8,68 @@ public class Student implements java.io.Serializable {
 	private String classnum;
 	private boolean attend;
 	private School school;
-	private int schoolyear;
+	
 	
 	public String getNo() {
 		return no ;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getEntYear() {
-		return entyear;
-	}
-	
-	public String getClassNum() {
-		return classnum;
-	}
-	
-	public boolean isAttend() {
-		return attend;
-	}
-	
-	public School getSchool() {
-		return school;
-	}
-	
-	public School getSchoolYear() {
-		return school;
-	}
-//	ここまで出来た
-	
-	public void setName(String name) {
-		this.name=name;
 	}
 	
 	public void setNo(String no) {
 		this.no=no;
 	}
 	
-	public void setEntYear(Int entYear) {
-		this.entyear=entyear;
+	public String getName() {
+		return name;
 	}
 	
-	public void setClassNum(String classNum) {
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public int getEntYear() {
+		return entyear;
+	}
+	
+	public void setEntYear(int entyear) {
+		this.entyear=entyear;
+	}
+
+	public String getClassNum() {
+		return classnum;
+	}
+	
+	public void setClassNum(String classnum) {
 		this.classnum=classnum;
+	}
+	
+	public boolean isAttend() {
+		return attend;
 	}
 	
 	public void setAttend(Boolean attend) {
 		this.attend=attend;
 	}
 	
+	public School getSchool() {
+		return school;
+	}
+	
 	public void setSchool(School school) {
 		this.school=school;
 	}
+	
+	public School getSchoolYear() {
+		return school;
+	}
+	
+
+
+	
+
+	
+
+	
+
+	
+
 }
