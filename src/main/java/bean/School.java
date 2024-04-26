84ba2231e5ce28914243b97eb462752dc1a9b1cc
t_asano <t_asano@LAPTOP-6LFOSQ8U>
2024-04-26 10:10:06 +0900

    変更

--- src/main/java/bean/School.java
+++ src/main/java/bean/School.java
@@ -1,29 +1,5 @@
 package bean;
 
-<<<<<<< HEAD
-public class School implements java.io.Serializable{
-	
-	private String name;
-	private String cd;
-
-	public String getCd() {
-		return cd;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setCd(String cd) {
-		this.cd=cd;
-	}
-	
-	public void setName(String name) {
-		this.name=name;
-	}
-}    
-
-=======
 public class School implements java.io.Serializable {
     
     private String name; // 学校の名前を格納するフィールド
@@ -46,4 +22,3 @@ public class School implements java.io.Serializable {
         this.cd = cd; // 学校のコードを設定
     }
 }
->>>>>>> branch 'master' of https://github.com/MinseokGom/Exam2.git
--- src/main/java/bean/Teacher.java
+++ src/main/java/bean/Teacher.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 package bean;
 
 public class Teacher implements java.io.Serializable {
@@ -42,47 +40,3 @@ public class Teacher implements java.io.Serializable {
 	}
 	
 }
-=======
-package bean;
-
-public class Teacher implements java.io.Serializable {
-   
-	private String id;
-	private String name;
-	private String password;
-	private School school;
-	
-	public String getId() {
-		return id ;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public School getSchool() {
-		return school;
-	}
-	
-	public void setId(String id) {
-		this.id=id;
-	}
-	
-	public void setName(String name) {
-		this.name=name;
-	}
-	
-	public void setPassword(String password) {
-		this.password=password;
-	}
-	
-	public void setSchool(School school) {
-		this.school=school;
-	}
-	
-}
->>>>>>> branch 'master' of https://github.com/MinseokGom/Exam2.git
