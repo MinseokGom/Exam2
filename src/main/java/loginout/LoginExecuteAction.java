9807726a75070c6b7cb9e09cb22f539b00f7386e
m_takayama <m_takayama@LAPTOP-BH4R2DGN>
2024-04-26 10:27:23 +0900

    do

---
+++ src/main/java/loginout/LoginExecuteAction.java
@@ -0,0 +1,36 @@
+
+package loginout;
+
+import bean.Teacher;
+import dao.TeacherDAO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+//ActionにLoginActionを継承する
+public class LoginExecuteAction extends Action{
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response
+			)throws Exception {
+		
+		HttpSession session=request.getSession();
+		
+		String id=request.getParameter("id");
+		String password=request.getParameter("password");
+		TeacherDAO dao=new TeacherDAO();
+		Teacher teacher=dao.login(id,password);
+		
+		if (teacher!=null) {
+			session.setAttribute("teacher",teacher);
+			return "../menu.jsp";
+		}
+		else {
+			return "login-error.jsp";
+		}
+		
+		
+	}
+	
+  
+}
