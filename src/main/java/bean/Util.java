a04889443465a15fb9beecb9022458747dfe29d5
m_takayama <m_takayama@192.168.10.116>
2024-05-01 14:00:56 +0900

    誰か助けて

--- src/main/java/bean/Util.java
+++ src/main/java/bean/Util.java
@@ -8,11 +8,17 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public class Util implements java.io.Serializable{
 	 public Teacher getUser(HttpServletRequest request) {
-	        // ここにユーザー情報を取得するロジックを実装してください
+	        // ユーザー情報を取得するロジックを実装
+		 
+		 
 	        // 例えば、セッションからユーザー情報を取得するなど
 
 	        // この部分は適切なロジックに置き換えてください
 	        return null;
+	        
+	        
+	        
+	        
 	    }
      public void setClassNumSet(HttpServletRequest request)throws ServletException, IOException{
     	// ここにsetClassNumSetメソッドの処理を記述してください
@@ -21,5 +27,21 @@ public class Util implements java.io.Serializable{
 
          // この部分は適切なロジックに置き換えてください
         }
-     public void setEntYearSet();
+     public void setEntYearSet(HttpServletRequest request)throws ServletException, IOException {
+    	// ここにメソッドの処理を記述してください
+         // reqを使って必要な操作を実装してください
+         // 例えば、データベースへのクエリ実行やリクエストパラメータの取得など
+
+         // この部分は適切なロジックに置き換えてください
+     }
+     
+     public void setSubjects(HttpServletRequest request)throws ServletException, IOException{
+    	 
+     }
+     
+     public void setNumSet(HttpServletRequest request)throws ServletException,IOException{
+    	 
+     }
+     
+     
 }
