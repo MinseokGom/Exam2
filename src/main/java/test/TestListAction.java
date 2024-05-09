c73d1cdbf3199c3e07a994dc3595e6dd6712ae00
t_asano <t_asano@LAPTOP-6LFOSQ8U>
2024-05-09 12:10:25 +0900

    衝突したらドンマイ

--- src/main/java/test/TestListAction.java
+++ src/main/java/test/TestListAction.java
@@ -9,16 +9,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import tool.Action;
 
 public class TestListAction extends Action{
-	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException{
 		
 	}
 	
 	public void setTestListSubject(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException{
-		
+		return "test_regist.jsp";
 	}
 	
 	public void setTestListStudent(HttpServletRequest req ,HttpServletResponse res)throws ServletException,IOException{
 		
+	}
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
 	}
 }
