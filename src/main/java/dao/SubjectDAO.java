6db08ff25527fc805244a6199290c30fe3a83ca6
n_oka <n_oka@10.234.146.51>
2024-05-15 12:08:07 +0900

    QWER saiko

--- src/main/java/dao/SubjectDAO.java
+++ src/main/java/dao/SubjectDAO.java
@@ -3,8 +3,6 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import bean.School;
@@ -12,88 +10,23 @@ import bean.Subject;
 
 public class SubjectDAO extends DAO {
 
-<<<<<<< HEAD
-    public List<Subject> filter(School school) throws Exception {
-        List<Subject> subjects = new ArrayList<>();
-        Connection con = null;
-        PreparedStatement st = null;
-        ResultSet rs = null;
-
-        try {
-            con = getConnection();
-            String sql = "SELECT * FROM subject WHERE school_cd = ?";
-            st = con.prepareStatement(sql);
-            st.setString(1, school.getCd());
-            rs = st.executeQuery();
-
-            while (rs.next()) {
-                Subject subject = new Subject();
-                subject.setCd(rs.getString("cd"));
-                subject.setName(rs.getString("name"));
-                subject.setSchool(school);
-                subjects.add(subject);
-            }
-        } catch (SQLException e) {
-            throw new Exception("Failed to filter subjects.", e);
-        } finally {
-            if (rs != null) rs.close();
-            if (st != null) st.close();
-            if (con != null) con.close();
-        }
-
-        return subjects;
+	public Subject get(List<Subject> woo) throws Exception {
+            Connection con = getConnection();
+            
+            for (Subject subject:woo) {
+            PreparedStatement st=con.prepareStatement(
+    			"SELECT * FROM subject WHERE school_cd = ? AND cd = ? AND  name = ?");
+            School sch = Subject.getSchool();
+            st.setString(1,sch.getCd());
+            st.setString(2, Subject.getCd());
+            st.setString(3,Subject.getName());
+            ResultSet rs = st.executeQuery();
+        
+         rs.close(); 
+         con.close();
+        
+		return Subject;
     }
-=======
-//	public Subject get(List<Subject> woo) throws Exception {
-//            Connection con = getConnection();
-//            
-//            for (Subject subject:woo) {
-//            PreparedStatement st=con.prepareStatement(
-//    			"SELECT * FROM subject WHERE school_cd = ? AND cd = ? AND  name = ?");
-//            School sch = Subject.getSchool();
-//            st.setString(1,sch.getCd());
-//            st.setString(2, Subject.getCd());
-//            st.setString(3,Subject.getName());
-//            ResultSet rs = st.executeQuery();
-//        
-//         rs.close(); 
-//         con.close();
-//        
-//		return Subject;
-//    }
-//	}
-	public List<Subject> getAllSubjects() throws Exception {
-	    List<Subject> subjects = new ArrayList<>();
-	    Connection con = null;
-	    PreparedStatement pstmt = null;
-	    ResultSet rs = null;
-
-	    try {
-	        con = getConnection();
-	        String sql = "SELECT school_cd,cd,name  FROM subject";
-	        pstmt = con.prepareStatement(sql);
-	        rs = pstmt.executeQuery();
-
-	        while (rs.next()) {
-	            Subject subject = new Subject();
-
-	            subject.setCd(rs.getString("CD"));
-	            subject.setName(rs.getString("NAME"));
-	            subject.setSchool(rs.getSchool("school"));
-	            // 学校コードから学校の詳細を取得するメソッドがあると仮定します
-	            School school = getSchoolByCode(rs.getString("SCHOOL_CD")); 
-	            subject.setSchool(school);
-	            subjects.add(subject);
-	        }
-	    } catch (SQLException e) {
-	        throw new Exception("すべての科目を取得できませんでした。", e);
-	    } finally {
-	        if (rs != null) { rs.close(); }
-	        if (pstmt != null) { pstmt.close(); }
-	        if (con != null) { con.close(); }
-	    }
-
-	    return subjects;
 	}
 
 
@@ -130,5 +63,4 @@ public class SubjectDAO extends DAO {
 		return subjects.remove(subject);
 	}
 
->>>>>>> branch 'master' of https://github.com/MinseokGom/Exam2.git
 }
